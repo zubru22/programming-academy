36090004eb6bc01855c92c4b8c2383752a92ae05
Michal Zuber <michal.zuber@tieto.com>
2018-08-09 23:22:34 -0700

    Fixed bug in last test case - adjusted code to pass corrected test

--- omit_five/omit_five.c
+++ omit_five/omit_five.c
@@ -1,5 +1,7 @@
 #include <assert.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
 * Function takes a 'start' and 'end' number of a range, and should return the count of all numbers except numbers with 5 in it.
 * For example: start = 4, end = 8. It should return 4, because: 4, 6, 7, 8
@@ -7,19 +9,35 @@
 int numbers_without_five(int start, int end)
 {
 	int count = 0;
+	int end_copy = end;
+	int digits = 0;
+	char *num_str;
+	
+	// Calculate maximum number of digits to be stored inside the string
+	while (end_copy != 0)
+	{
+		end_copy /= 10;
+		digits++;
+	}
+	
+	// Allocate memory for string to store converted integer
+	num_str = (char *) malloc(digits * sizeof(char) + 1);
 	
 	// Iterate in range 
 	for (int i = start; i <= end; i++)
 	{
-		/* If it's possible to divide number by 5 without reminder
-		 * and the number is not even then it has 5 in it */
-		if (i % 5 == 0 && i % 2 != 0)
+		sprintf(num_str, "%d", i);
+		
+		// If string contains 5 skip it
+		if (strchr(num_str, '5'))
 			continue;
-		// Else it's just a number
+		// Else count the number
 		else
 			count++;
 	}
 	
+	// Free memory allocated for string
+	free(num_str);
 	return count;
 }
 
@@ -36,7 +54,7 @@ void test_cases()
 	
 	// Test case in bigger range
 	answer = numbers_without_five(1, 100);
-	assert(answer == 90);
+	assert(answer == 81);
 }
 
 int main(int argc, char *argv[])
