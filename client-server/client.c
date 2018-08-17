5a22f3081b36378756fe9a47e4e42be050d380c9
Michal Zuber <michal.zuber@tieto.com>
2018-08-17 05:30:28 -0700

    Bugg epoll server

--- client-server/client.c
+++ client-server/client.c
@@ -1,34 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+// Display error message if system call fails
+void error(char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+// Gets text from client, it can be either username or message to send
+void get_text(char *promp_msg, char *string, int len)
+{
+	printf("%s", promp_msg);
+	fgets(string, len, stdin);
+	strtok(string, "\n");
+}
 
 int main()
 {
-	// create socket
+	const int user_len = 30;
+	const int message_len = 128;
+	char user_name[user_len];
+	char message[message_len];
 	int network_socket;
+	int connection_status;
+	struct sockaddr_in server_address;
+	
+	// Specify server's socket
 	network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (network_socket == -1)
+		error("Could not create network_socket");
 	
 	// Specify an address for the socket
-	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(2000);
+	server_address.sin_port = htons(9000);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	
-	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	connection_status = connect(network_socket, (struct sockaddr *) &server_address, (int) sizeof(server_address));
 	// Check for error with the connection
 	if (connection_status == -1)
-		printf("There was an error making a connection to the remote socket\n");
-		
-	// Receive data from the server
-	char server_response[256];
-	recv(network_socket, &server_response, sizeof(server_response), 0);
+	{
+		printf("Errno: %d\n", errno);
+		error("Error while connecting.");
+	}	
 	
-	// Print out the server's response
-	printf("The server sent the data: %s\n", server_response);
+	get_text("Type in your username: ", user_name, sizeof(user_name));
+	
+	printf("Type in QUIT to quit.\n");
+	while (strcmp(message, "QUIT"))
+	{
+		get_text("Message: ", message, sizeof(message));
+		send(network_socket, message, sizeof(message), 0);
+	}
 	
 	// Close the socket
 	close(network_socket);
--- client-server/server.c
+++ client-server/server.c
@@ -1,11 +1,15 @@
 /* A simple server in the internet domain using TCP. The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
-
+#include <sys/epoll.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MAX_EVENTS 10
 
 // Display error message if system call fails
 void error(char *msg)
@@ -14,30 +18,82 @@ void error(char *msg)
 	exit(1);
 }
 
+void do_use_fd(int socket)
+{
+	printf("Goodbye!\n");
+}
+
 int main()
 {
-	char server_message[256] = "You have reached the server!";
+	const int message_len = 256;
+	char user_says[message_len];
+	int server_socket;
+	int client_socket;
+	int epoll_fd;
+	int nfds;
+	int status;
+	struct sockaddr_in server_address;
+	struct epoll_event ev;
+	struct epoll_event events[MAX_EVENTS];
 	
 	// Create the server socket
-	int server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
 	// Define the server address
-	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(2000);
+	server_address.sin_port = htons(9000);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	
 	// Bind the socket to our specified IP and port
-	bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	bind(server_socket, (struct sockaddr *) &server_address, (int) sizeof(server_address));
 	
 	listen(server_socket, 5);
 	
-	int client_socket;
-	client_socket = accept(server_socket, NULL, NULL);
+	// Create epoll file descriptor
+	epoll_fd = epoll_create1(0);
+	if (epoll_fd == -1)
+		error("Error while creating epoll_fd");
+		
+	ev.events = EPOLLIN;
+	ev.data.fd = server_socket;
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_socket, &ev) == -1)
+		error("Error epoll_ctl: server_socket");
 	
-	// Send the message
-	send(client_socket, server_message, sizeof(server_message), 0);
+	while(1)
+	{
+		nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+		if (nfds == -1)
+			error("Error with epoll_wait");
+			
+		for (int n = 0; n < nfds; ++n)
+		{
+			if (events[n].data.fd == server_socket)
+			{	
+				// (struct sockaddr *) &client_address, sizeof(struct sockaddr_in)
+				client_socket = accept(server_socket, NULL, NULL);
+				if (client_socket == -1)
+				{
+					printf("Errno: %d\n", errno);
+					error("Error with accept");
+				}	
+				status = fcntl(client_socket, F_SETFL, fcntl(client_socket, F_GETFL, 0) | O_NONBLOCK);
+				if (status == -1)
+					error("Error while calling fcntl");
+				
+				ev.events = EPOLLIN | EPOLLET;
+				ev.data.fd = client_socket;
+				if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &ev) == -1)
+					error("Error with epoll_ctl: client_socket");
+	
+			}
+			else
+			{
+				recv(client_socket, &user_says, sizeof(user_says), 0);
+				//recv(client_socket, &user_message, sizeof(user_message), 0);
+				printf("%s\n", user_says);
+			}
+		}
+	}
 	
 	// Close the socket
 	close(server_socket);
