497fceff9ac6aa1bc1e64232e45d5d497b3caa03
Michal Zuber <michal.zuber@tieto.com>
2018-08-16 23:18:41 -0700

    Simplify both client and server files

--- client-server/client.c
+++ client-server/client.c
@@ -1,68 +1,35 @@
-/* A simple server in the internet domain using TCP. The port number is passed as an argument */
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-// Display error message if system call fails
-void error(char *msg)
+int main()
 {
-	perror(msg);
-	exit(1);
-}
-
-int main(int argc, char *argv[])
-{
-	int sockfd, portno, n;
-	struct sockaddr_in serv_addr; // will contain the address of the server to which we want to connect.
-	struct hostent *server; // it defines host computer on the Internet
+	// create socket
+	int network_socket;
+	network_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
-	/* All this code is the same as that in the server */
-	char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    
-    /* argv[1] contains the name of a host on the Internet, e.g. cheerios@cs.rpi.edu 
-     * hostent takes a name as argument and returns a pointer to hostent containing information about that host.
-     * The field char *h_addr contains the IP address.
-     * If this structure is NULL, the system could not locate a host with this name. */    
-	server = gethostbyname(argv[1]);
-	if (server == NULL)
-	{
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
-	}
+	// Specify an address for the socket
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(2000);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 	
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length); // server->h_addr is string so we use bcopy()
-	serv_addr.sin_port = htons(portno);
-	
-	/* The connect function is called by the client to establish a connection to the server.
-	 * It takes three arguments, socket file descriptor, the address of the host to which it wants to connect (including port number)
-	 * and the size of this address. Function return 0 on success and -1 if it fails */
-	if (connect(sockfd, &serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR connecting");
+	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	// Check for error with the connection
+	if (connection_status == -1)
+		printf("There was an error making a connection to the remote socket\n");
 		
-	printf("Please enter the message: ");
-	bzero(buffer, 256);
-	fgets(buffer, 255, stdin);
-	n = write(sockfd, buffer, strlen(buffer));
+	// Receive data from the server
+	char server_response[256];
+	recv(network_socket, &server_response, sizeof(server_response), 0);
 	
-	if (n < 0)
-		error("ERROR writing to socket");
-		
-	printf("%s\n", buffer);
+	// Print out the server's response
+	printf("The server sent the data: %s\n", server_response);
 	
-	return 0;
+	// Close the socket
+	close(network_socket);
 }
--- client-server/server.c
+++ client-server/server.c
@@ -1,11 +1,11 @@
 /* A simple server in the internet domain using TCP. The port number is passed as an argument */
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
 
 // Display error message if system call fails
 void error(char *msg)
@@ -14,73 +14,33 @@ void error(char *msg)
 	exit(1);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-	int sockfd, newsockfd; // file descriptors
-	int portno; // port number on which server accepts connections
-	int clilen; // size of address of the client, needed for the accept system call
+	char server_message[256] = "You have reached the server!";
 	
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
+	// Create the server socket
+	int server_socket;
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
-	if (argc < 2)
-	{
-		fprintf(stderr, "ERROR, no port provided\n");
-		exit(1);
-	}
+	// Define the server address
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(2000);
+	server_address.sin_addr.s_addr = INADDR_ANY;
 	
-	// System call to create new socket. AF_INET is internet domain, SOCK_STREAM is type of socket, third argument is the protocol chosen by operating system
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	// Bind the socket to our specified IP and port
+	bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 	
-	if (sockfd < 0)
-		error("ERROR opening socket");
+	listen(server_socket, 5);
 	
-	// Sets all values in a buffer to zero, first argument is pointer to the buffer and second is the size of the buffer
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	int client_socket;
+	client_socket = accept(server_socket, NULL, NULL);
 	
-	// The port number on which server will listen for connections (passed as argument), converted from string to integer
-	portno = atoi(argv[1]);
+	// Send the message
+	send(client_socket, server_message, sizeof(server_message), 0);
 	
-	serv_addr.sin_family = AF_INET; // should be always set to symbolic constant AF_INET
-	serv_addr.sin_port = htons(portno); // contains port number, it's necessary to convert this to network byte order using htons()
-	/* Contains IP address of the host. For server code this will always be the IP address of the machine on which the server is running 
-	 * and there is symbolic constant INADDR_ANY which gets this address */
-	serv_addr.sin_addr.s_addr = INADDR_ANY; 
+	// Close the socket
+	close(server_socket);
 	
-	/* System call binds socket to an address, in this case the address of current host and port number on which the server will run.
-	 * It takes three arguments, the socket file descriptor, the address to which is bound and the size of the address to which it is bound.
-	 * This can fail for number of reason but most obvious being that this socket is already in use on this machine. */
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
-		
-	 /* System call allows the proccess to listen on the socket for connections. 
-	  * Second argument is the size of backlog queue - the number of connections that can be waiting while the process is handling a particular connection. 
-	  * This should be set to 5, the maximum size permitted by most systems.
-	  * If the first argument is valid socked this call cannot fail so the code doesn't check for errors */
-	listen(sockfd, 5);
-	
-	/* The accept call causes the process to block until a client connects to the server. 
-	 * Thus it wakes up the process when a connection from a client has been successfully established.
-	 * It returns new file descriptor and all communication on this connection should be done using the new file descriptor.
-	 * Second argument is reference pointer to the address of the client on the other end of the connection.
-	 * Third argument is the size of this structure */
-	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	if (newsockfd < 0)
-		error("ERROR on accept");
-		
-	/* Once a connection has been established, both ends can both read and write to the connection.
-	 * Naturally, everything written by the client will be read by the server,
-	 * and everything written by the server will be read by the client.
-	 * This code simply writes a short message to the client. 
-	 * The last argument of write is the size of the message */
-	bzero(buffer, 256);
-	n = read(newsockfd, buffer, 255);
-	if (n < 0)
-		error("ERROR writing to socket.");
-	
-	printf("Here is the message: %s\n", buffer);
-		
 	return 0;
 }
