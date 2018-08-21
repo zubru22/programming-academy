2b6e55aed4bd8cb410b227f7833bcb53b21a3615
Zuber Michal <michal.zuber@tieto.com>
2018-08-21 15:44:25 +0200

    Change [released] attribute from eg. 12 Feb 2016 to 12.02.2016

--- python/imdb.py
+++ python/imdb.py
@@ -1,10 +1,12 @@
 import omdb
+from datetime import datetime
 omdb.set_default('apikey', '68f39148')
 # movie = omdb.get(title='Guardians of the Galaxy Vol. 2')
 
 
 def get_movie(movie):
     movie_stats = omdb.get(title=movie)
+    movie_stats['released'] = datetime.strptime(movie_stats['released'], '%d %b %Y').strftime("%d.%m.%Y")
     if (movie_stats):
         for key, value in movie_stats.items():
                 print(f'{key}: {value}')
